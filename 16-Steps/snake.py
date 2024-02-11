5dca5b105b0f2597a628e309bc59a6353fc6a2bc
Nabin Katwal <141636899+cyrolux123@users.noreply.github.com>
2024-02-11 18:48:44 +0545

    Add files via upload

---
+++ 16-Steps/snake.py
@@ -0,0 +1,50 @@
+from turtle import Turtle,Screen
+
+STARTING_POS = ((0, 0), (-20, 0), (-40, 0))
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+
+class Snake:
+
+    def move(self):
+        for idx in range(len(self.segments) - 1, 0, -1):
+            pos = self.segments[idx - 1].pos()
+            self.segments[idx].goto(pos)
+        self.head.fd(MOVE_DISTANCE)
+        print("Snake moved")
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POS:
+            self.segments.append(Turtle('square'))
+            self.segments[-1].color('white')
+            self.segments[-1].pu()
+            self.segments[-1].goto(pos)
+
+    def move(self):
+        for idx in range(len(self.segments) - 1, 0, -1):
+            pos = self.segments[idx - 1].pos()
+            self.segments[idx].goto(pos)
+        self.head.fd(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
